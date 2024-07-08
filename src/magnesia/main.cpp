007038b1f4ed8010107032c09f53b15deeaafa8f
Marwin Glaser <uyygb@student.kit.edu>
2024-07-08 22:23:52 +0200

    style(main): reformat with new IncludeCategories
    Also remove unused include. The build is already tested by including and
    using <open62541pp/Server.h>.

--- src/magnesia/main.cpp
+++ src/magnesia/main.cpp
@@ -1,9 +1,8 @@
+#include <open62541pp/Server.h>
+
 #include <QApplication>
 #include <QLabel>
 #include <qnamespace.h>
-
-#include <open62541/server.h> // Just included to test build
-#include <open62541pp/Server.h>
 
 int main(int argc, char** argv) {
     QApplication app(argc, argv);
