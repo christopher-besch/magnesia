ffb2d3ef3d8bd2da1162876e514a2242fe61c9e1
Marwin Glaser <uyygb@student.kit.edu>
2024-07-17 19:55:31 +0200

    ci(lint): remove workarounds for open62541 and include-cleaner
    These are no longer necessary with nix now providing open62541 and
    recent versions of the other dependencies.

--- .ci/lint.sh
+++ .ci/lint.sh
@@ -18,14 +18,8 @@ run_clang_tidy() {
         -D MAGNESIA_WARNINGS_AS_ERRORS=ON \
         -D MAGNESIA_BUILD_DOCS=OFF
 
-    # NOTE: open62541 generates headers during build. This means we have to build open62541 for clang-tidy to find all
-    # includes. This is not an issue if the library is provided by the system.
-    cmake --build build/clang-tidy --target libopen62541.a || true
-
-    # NOTE: The Qt version in ubuntu (6.4) is too old and doesn't yet contain the "IWYU pragma: export" in the public
-    # headers, so don't treat false-positive include-cleaner warnings as errors for now.
     find src -name '*.[ch]pp' -print0 \
-        | xargs -0 run-clang-tidy -warnings-as-errors='*,-misc-include-cleaner' -use-color -p build/clang-tidy
+        | xargs -0 run-clang-tidy -warnings-as-errors='*' -use-color -p build/clang-tidy
 }
 
 main() {
