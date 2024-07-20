f9d27230560f2ae31fc73ed32926cd7f17d7db9f
Marwin Glaser <uyygb@student.kit.edu>
2024-07-20 11:51:55 +0200

    ci(scripts): allow running multiple jobs in parallel in the same folder

--- .ci/build.sh
+++ .ci/build.sh
@@ -2,7 +2,8 @@
 
 set -xeu
 
-BUILD_DIR="$(pwd)/build/ci"
+mkdir -p build
+BUILD_DIR="$(mktemp -dp build ci.XXXXXXXXX)"
 trap 'rm -rf "'"$BUILD_DIR"'"' EXIT
 
 cmake -G Ninja -B "$BUILD_DIR" -DCMAKE_BUILD_TYPE="$BUILD_TYPE"
