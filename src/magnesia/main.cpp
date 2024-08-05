e01d841549c5534f60d2ee3ed6756122305beff6
Marwin Glaser <uyygb@student.kit.edu>
2024-08-05 04:45:05 +0200

    feat: update qt logging format to include time and severity

--- src/magnesia/main.cpp
+++ src/magnesia/main.cpp
@@ -1,8 +1,17 @@
 #include "Application.hpp"
+#include "qt_version_check.hpp"
 
 #include <QApplication>
 
+#ifdef MAGNESIA_HAS_QT_6_5
+#include <QtLogging>
+#else
+#include <QtGlobal>
+#endif
+
 int main(int argc, char** argv) {
+    qSetMessagePattern("%{time yyyy-MM-ddTHH:mm:ss.zzzttt} %{if-category}%{category} %{endif}%{type}: %{message}");
+
     const QApplication qapp(argc, argv);
 
     const magnesia::Application app;
