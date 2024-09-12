3c6357f7e63397055004ab4a0647a6ff3598b8bc
Christoph Wolf <ufnsu@student.kit.edu>
2024-09-12 19:17:59 +0200

    fix(dataviewer/node): remove table border

--- src/magnesia/activities/dataviewer/panels/NodeViewPanel.cpp
+++ src/magnesia/activities/dataviewer/panels/NodeViewPanel.cpp
@@ -10,6 +10,7 @@
 #include <algorithm>
 
 #include <QAbstractItemView>
+#include <QFrame>
 #include <QHeaderView>
 #include <QItemSelectionModel>
 #include <QModelIndex>
@@ -30,6 +31,7 @@ namespace magnesia::activities::dataviewer::panels::node_view_panel {
         m_table_view->verticalHeader()->setSectionResizeMode(QHeaderView::ResizeToContents);
         m_table_view->setSelectionBehavior(QAbstractItemView::SelectRows);
         m_table_view->setSelectionMode(QAbstractItemView::ContiguousSelection);
+        m_table_view->setFrameShape(QFrame::Shape::NoFrame);
 
         auto* layout = new QVBoxLayout;
         layout->addWidget(m_table_view);
